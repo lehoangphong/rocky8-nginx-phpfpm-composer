d5384bce8c70cd767f3c1b7a1be2a8660d6830e2
lehoangphong <lehoangphong1487@gmail.com>
2024-01-24 06:46:02 +0700

    add prometheus metrics

--- web/index.php
+++ web/index.php
@@ -1,3 +1,22 @@
 <?php
 include('Authen.inc');
 echo get_include_path();
+
+require 'vendor/autoload.php';
+
+use Prometheus\CollectorRegistry;
+use Prometheus\Storage\InMemory;
+
+$adapter = new InMemory();
+$registry = new CollectorRegistry($adapter);
+
+$counter = $registry->registerCounter('example', 'requests_total', 'help', ['method']);
+$counter->incBy(1, ['GET']);
+
+if ($_SERVER['REQUEST_URI'] === '/metrics') {
+    header('Content-Type: text/plain');
+    echo $registry->render();
+    exit;
+}
+
+echo "Hello, World!";
